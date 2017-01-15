60b5f8d3871beb7fd27610d9714a8febc2c87cbc
Szilard Magyar <szilard.magyar@gmail.com>
2017-01-15 13:19:50 +0100

    refactoring board display

--- player.rb
+++ player.rb
@@ -1,6 +1,8 @@
 class Player
   attr_reader :marker
 
+  ROW_MAP = (:A..:Z).zip(0..25).to_h
+
   def initialize(name, marker)
     @name = name
     @marker = marker
@@ -8,14 +10,35 @@ class Player
   end
 
   def get_move
-    [get_row, get_col]
+    #[get_row, get_col]
+    [get_coordinate("row"), get_coordinate("column")]
   end
 
-  def get_row
-    gets.to_i
-  end
+  private
 
-  def get_col
-    gets.to_i
-  end
+    def get_coordinate(row_or_column)
+      loop do
+        print choice_prompt(row_or_column)
+        input = gets
+        choice = send("translate_#{row_or_column}".to_sym, input)
+        return choice if valid_choice?(choice)
+        puts "Invalid #{row_or_column}"
+      end
+    end
+
+    def translate_row(input)
+      ROW_MAP[input[0].upcase.to_sym]
+    end
+
+    def translate_column(input)
+      input.to_i - 1
+    end
+
+    def valid_choice?(choice)
+      choice if (0..2).include?(choice)
+    end
+
+    def choice_prompt(row_or_column)
+      "pick a #{row_or_column}\n> "
+    end
 end
